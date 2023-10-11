d79b72e1a2bad5b137ab99e6f5a7b9cb34c47c7c
dtieshue <dtieshue@gmail.com>
2023-10-10 22:27:16 -0400

    scraping file

---
+++ noaa_scraping.py
@@ -0,0 +1,198 @@
+import requests
+from bs4 import BeautifulSoup
+import re
+import pandas as pd
+
+# Function to fetch and extract text content from a URL
+def extract_text_from_url(url, start_keyword, end_keyword):
+    response = requests.get(url)
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, 'html.parser')
+
+        # Extract the text content from the HTML
+        text_content = soup.get_text().upper()
+
+        # Find the position of the start keyword and end keyword in the text
+        start_keyword_position = text_content.find(start_keyword)
+        end_keyword_position = text_content.find(end_keyword)
+
+        if start_keyword_position != -1 and end_keyword_position != -1:
+            # Extract and display text content starting from the start keyword
+            # up to just before the end keyword position
+            relevant_text = text_content[start_keyword_position:end_keyword_position]
+            return relevant_text
+        else:
+            print(f'Start or end keyword not found in {url}')
+            raise Exception("Keyword error")
+            return None
+    else:
+        print(f'Failed to fetch content from {url}')
+        return None
+
+# create dictionary to store all of the info
+master_dict = {}
+
+# loop through each year *** NO 2023, no pre-2003
+for year in range(2020, 2022):
+
+  # provide website data of NOAA archive for each year
+  url = f'https://www.nhc.noaa.gov/archive/{year}'
+  print(year)
+  response = requests.get(url)
+
+  # getting the links to each storm for a given year, based on a pattern in the position of the links
+  if response.status_code == 200:
+    soup = BeautifulSoup(response.text, 'html.parser')
+    links = soup.find_all('a')
+    found_arc1 = False
+    found_arc2 = False
+    found_cyc = False
+    storm_urls = []
+    for link in links:
+      # print(link.get('href'))
+      if found_cyc:
+        break;
+      elif link.get('href') == '/cyclones/':
+        found_cyc = True
+        break;
+      elif found_arc2:
+        # print(link.get('href'))
+        href = link.get('href')
+        storm_urls.append(f'https://www.nhc.noaa.gov/archive/{year}/{href}')
+      elif found_arc1 & (link.get('href') == '/archive/2023/') :
+        found_arc2 = True
+      elif link.get('href') == '/archive/2023/':
+        found_arc1 = True
+      else:
+        continue;
+  else:
+    print('Failed to retrieve the webpage.')
+
+  storm_name = ""
+  # loop through each storm for a given year
+  for url in storm_urls:
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, 'html.parser')
+
+        # if needed use code to extract a specific pattern for the links
+        links = soup.find_all('a', href=re.compile(r''))
+        adv_urls = []
+
+        for link in links:
+          href = link.get('href')
+          if href != None:
+            if href.__contains__('discus'):
+              full_url = f"https://www.nhc.noaa.gov{href}"
+              adv_urls.append(full_url)
+              print(full_url)
+
+    else:
+        print('Failed to retrieve the webpage.')
+
+
+    # list of all advisory discussion urls
+    urls_to_read = adv_urls
+
+    # specify words to start and end with in the text
+    start_keyword = "DISCUSSION"
+    end_keyword = f" {year}"
+    start_keyword2 = "FORECAST POSITIONS AND MAX WINDS"
+    end_keyword2 = "$"
+
+    storm_dict = {}
+    i = 1
+
+
+    # loop through the discussion urls
+    for url in urls_to_read:
+        name_datetime = extract_text_from_url(url, start_keyword, end_keyword)
+        loc_speed = extract_text_from_url(url, start_keyword2, end_keyword2)
+
+        name_datetime_arr = name_datetime.split("\n")
+        loc_speed_arr = loc_speed.split("\n")[2:]
+        while("" in loc_speed_arr):
+          loc_speed_arr = [s for s in loc_speed_arr if s.strip()]
+
+        disc_num = name_datetime_arr[0]
+        disc_num = re.sub(r'[^0-9]', '', disc_num)
+        # disc_num = disc_num[-1]
+
+
+        line2 = name_datetime_arr[1].split()
+        storm_name = line2[-1]
+        print(storm_name)
+
+        print(f'Content from {url}:\n')
+        if name_datetime:
+            print(f'Date/Time: \n{name_datetime_arr}\n')
+        if loc_speed:
+            # Process or print the text content as needed
+            print(f'Location/Direction/Speed:\n{loc_speed_arr}')
+
+        # storm_name = name_datetime
+
+        # initialize an empty list to store the data for a particular advisory
+        parsed_data = {}
+
+        # loop through each individual line in the selected string for a given advisory
+        for line in loc_speed_arr:
+            parts = line.split()
+
+            if parts[0] == "INITIAL":
+              parts[0] = "INIT"
+
+            # check if the line in the advisory is complete
+            if len(parts) >= 6:
+              time = parts[0]
+              date = parts[1]
+              lat = parts[2]
+              lon = parts[3]
+              wind_speed = parts[4]
+
+              # create a dictionary
+              data_dict = {
+                  'TOut': time,
+                  'Time (UTC)': date,
+                  'Latitude': lat,
+                  'Longitude': lon,
+                  'Wind Speed (KT)': wind_speed,
+                  'Dissipated?' : False
+                  # Add more key-value pairs here if needed
+              }
+
+
+            # check if a line indicates that the storm has dissipated
+            elif parts[1].find("DISSIPATED") != -1:
+              data_dict = {
+                  'TOut': parts[0],
+                  'Time (UTC)': parts[1][0:7],
+                  'Latitude': "n/a",
+                  'Longitude': "n/a",
+                  'Wind Speed (KT)': "n/a",
+                  'Dissipated?' : True
+              }
+
+
+              # add the data to the advisory dictionary
+            parsed_data[data_dict['TOut']] = data_dict
+
+        # add the advisory's dictionary information to the storm dictionary
+        # storm_dict[disc_num] = parsed_data
+        storm_dict[f'{i}'] = parsed_data
+        i = i + 1
+        print(parsed_data)
+
+
+
+  # add the storm's dictionary information to the master dictionary
+    master_dict[storm_name] = storm_dict
+    print("storm:")
+    print(storm_dict)
+
+
+
+################################################################################################################################################################################################################################################
+
+
